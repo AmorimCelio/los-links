32d5890939242f18e0456beba4b3d45f7e7c52e1
Célio Amorim <celio.amorim0111@gmail.com>
2024-11-08 20:08:38 -0300

    adicionando função link open

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useCallback } from "react"
-import { Image , View, TouchableOpacity, FlatList, Modal, Text, Alert} from "react-native"
+import { Image , View, TouchableOpacity, FlatList, Modal, Text, Alert, Linking} from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
 import { router, useFocusEffect } from "expo-router"
 
@@ -52,6 +52,16 @@ export default function Index() {
     
   }
 
+  async function handleOpen() {
+    try {
+      await Linking.openURL(link.url)
+      setShowModal(false)
+    } catch (error) {
+      Alert.alert("Link", "Não foi possível abrir o link")
+      console.log(error)
+    }
+  }
+
   useFocusEffect(useCallback(() => {
     getLinks()
   }, [category])
@@ -100,7 +110,7 @@ export default function Index() {
 
               <View style={styles.modalFooter}>
                 <Option name="Excluir" icon="delete" variant="secondary" onPress={handleRemove}/>
-                <Option name="Abrir" icon="language"/>
+                <Option name="Abrir" icon="language" onPress={handleOpen}/>
               </View>
           </View>
         </View>
